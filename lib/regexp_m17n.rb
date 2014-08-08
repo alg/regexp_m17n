b1f3027d882f2991778a8682cf0d6984edbec844
Aleksey Gureiev <spyromus@noizeramp.com>
2014-08-08 15:03:57 +0400

    my solution

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,10 @@
 module RegexpM17N
+  UTF8 = Encoding.find("UTF-8")
+
   def self.non_empty?(str)
-    str =~ /^.+$/
+    # Forcing doesn't encode, but changes the encoding.
+    # Since we don't care about the actual characters, just their presence,
+    # using this method is reasonable
+    str.to_s.force_encoding(UTF8) =~ /^.+$/
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,10 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      # 'encoding' should be replaced with 'force_encoding' to
+      # make test work with ISO-2022-JP-2 (dummy) encoding.
+      # There's no UTF-8 to ISO-2022-JP-2 converter.
+      assert(RegexpM17N.non_empty?('.'.force_encoding(enc)))
     end
   end
 end
